bf877f03f2393fd7468c19bd4f07bfd59be2ebd4
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2020-07-09 20:34:07 +0900

    JS: Navbar

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-node main.js 
+"use strict";
+
+// Make navbar tansparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
