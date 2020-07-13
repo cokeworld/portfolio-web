b5276bb3f71898369a17fa9484a4e341de5a5b97
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2020-07-13 21:18:56 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -28,10 +28,25 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
+//Make home slowly fede to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
 });
 
 function scrollIntoView(selector) {
