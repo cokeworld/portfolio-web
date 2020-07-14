03927b07cf669ea153057f760603edeea66b877b
Tony Kim <67273859+cokeworld@users.noreply.github.com>
2020-07-14 18:47:06 +0900

    JS:  Work filtering

--- main.js
+++ main.js
@@ -28,14 +28,14 @@ homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
 });
 
-//Make home slowly fede to transparent as the window scrolls down
+// Make home slowly fede to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-//Show "arrow up" button when scrolling down
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up");
 document.addEventListener("scroll", () => {
   if (window.scrollY > homeHeight / 2) {
@@ -45,8 +45,31 @@ document.addEventListener("scroll", () => {
   }
 });
 
+// Handle click on the "arrow up" button
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
+});
+
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
 
 function scrollIntoView(selector) {
